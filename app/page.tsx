50b8670c79ae2dc29690a717c110b9a942478128
sahil-raj <sahilrajaaru@gmail.com>
2024-10-28 02:32:07 +0530

    chore: init

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function Home() {
+  return (
+    <>
+      <DialogRoot
+        size="cover"
+        placement="center"
+        motionPreset="slide-in-bottom"
+      >
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm">
+            Open Dialog
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Dialog Title</DialogTitle>
+            <DialogCloseTrigger />
+          </DialogHeader>
+          <DialogBody>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </DialogBody>
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+}
