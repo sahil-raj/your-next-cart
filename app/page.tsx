4c3012249fc171496245f55e10e67ee70d964938
sahil-raj <sahilrajaaru@gmail.com>
2024-10-28 21:09:36 +0530

    chore: react suspense added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 // import ProductCard from "@/components/ProductCard";
 import InfiniteScroll from "./InfiniteScroll";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function Home() {
   return (
     <div className=" pt-[25vh]">
       <h1 className="main-heading text-center text-4xl">Product List</h1>
-      <InfiniteScroll></InfiniteScroll>
+      <Suspense fallback={<Loading />}>
+        <InfiniteScroll></InfiniteScroll>
+      </Suspense>
     </div>
   );
 }
